5cb01fed5ef65af221c0dbb3304564ea2e5a0938
ourluv <aorlowde@gmail.com>
2024-04-21 09:19:29 +0300

    added test for serializer

--- binary_serializer_test.go
+++ binary_serializer_test.go
@@ -1,8 +1,52 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBinarySerializer(t *testing.T) {
 	encoder := NewBinarySerializer()
-	_ = encoder
+	encoder.SerializeLengthUint15(0)          // [0]
+	encoder.SerializeLengthUint15(127)        // [127]
+	encoder.SerializeLengthUint15(128)        // [128, 128]
+	encoder.SerializeLengthUint15(32767)      // [255, 255]
+	encoder.SerializeLengthUint30(0)          // [0]
+	encoder.SerializeLengthUint30(63)         // [63]
+	encoder.SerializeLengthUint30(64)         // [64, 64]
+	encoder.SerializeLengthUint30(16383)      // [127, 255]
+	encoder.SerializeLengthUint30(16384)      // [128, 64, 0]
+	encoder.SerializeLengthUint30(4194303)    // [191, 255, 255]
+	encoder.SerializeLengthUint30(4194304)    // [192, 64, 0, 0]
+	encoder.SerializeLengthUint30(1073741823) // [255, 255, 255, 255]
+
+	assert.Equal(t, []byte{
+		0,
+		127,
+		128,
+		128,
+		255,
+		255,
+		0,
+		63,
+		64,
+		64,
+		127,
+		255,
+		128,
+		64,
+		0,
+		191,
+		255,
+		255,
+		192,
+		64,
+		0,
+		0,
+		255,
+		255,
+		255,
+		255,
+	}, encoder.output)
 }
